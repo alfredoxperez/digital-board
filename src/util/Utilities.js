5878db5f3d31e13c279b3db6c93d4c9b3cd304b3
Alfredo Perez <alfredoxperez@gmail.com>
2018-01-30 11:00:04 -0500

    Utilities.js: adding functions to strike and unStrike text

--- src/util/Utilities.js
+++ src/util/Utilities.js
@@ -9,5 +9,11 @@
 module.exports = {
   copyObject: function(json: Object) {
     return JSON.parse(JSON.stringify(json))
+  },
+  strikeText: function(str: String){
+    return str.replace(/./g, (chr) => {return chr + '\u0336'})
+  },
+  unStrikeText: function(str: String){
+    return str.replace(/[^\x00-\x7F]/g, "")
   }
 }
