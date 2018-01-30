ea1414bb1069fae07802c5abe5e9acad700860bb
Alfredo Perez <alfredoxperez@gmail.com>
2018-01-30 12:47:24 -0500

    UI: tweeking look plus adding strike functionality

--- public/js/digital-board-logic.js
+++ public/js/digital-board-logic.js
@@ -8,7 +8,7 @@ socket.on('connect', () => {
     data: '',
     success: function (data) {
       data.forEach(d => 
-        $('#digital-board').val(d + '\n' + $('#digital-board').val()))
+        $('#digital-board').val(d.boardText + '\n' + $('#digital-board').val()))
     },
     error: function (xhr, ajaxOptions, thrownError) {
       console.log("digital-board-logic.js: error: ", xhr.statusText)
@@ -16,8 +16,19 @@ socket.on('connect', () => {
   })
 })
 
-socket.on('outboundDigitalboardSocket', data => 
-  $('#digital-board').val(data + '\n' + $('#digital-board').val()))
+socket.on('outboundDigitalboardSocket', data => {
+  //if new entry then just add it to the top...
+  if(data.row == -1) {
+    $('#digital-board').val(data.boardText + '\n' + $('#digital-board').val())
+  } else {
+    //else we have an edited entry and need to insert it and update textarea...
+    let textLines = $('#digital-board').val().substr(0).split("\n")
+    textLines[data.row-1] = data.boardText
+    
+    $('#digital-board').val(textLines
+      .reduce((accumulator, currentValue) => accumulator + '\n' + currentValue))
+  }
+})
 
 
 function checkEnter(e){ 
@@ -63,4 +74,37 @@ function submitText() {
       }
     })
   }
+}
+
+
+let clickTimer = null
+function checkTextAreaClick(textarea) {
+  if (clickTimer == null) {
+    //Single Tap
+    clickTimer = setTimeout( () => {clickTimer = null}, 500)
+  } else {
+    //Double Tap
+    clearTimeout(clickTimer)
+    clickTimer = null
+    strikethroughText(textarea)
+  }
+}
+
+function strikethroughText(textarea){
+  let textLines = textarea.value.substr(0, textarea.selectionStart).split("\n")
+  let position = {
+    row: textLines.length,
+    column: textLines[textLines.length-1].length
+  }
+  $.ajax({
+    url: 'http://localhost:3000/digital-board/api/1.0/text',
+    contentType: 'application/json',
+    type: 'POST',
+    data: JSON.stringify(position),
+    success: function (response) {
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+      console.log("digital-board-logic.js: error: ", xhr.statusText)
+    }
+  })
 }
